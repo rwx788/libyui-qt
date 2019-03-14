fc2bacf6fe038484b802759267d08585fad64c4b
Rodion Iafarov <riafarov@suse.com>
2019-03-14 21:03:50 +0100

    Fix version

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,6 +6,6 @@ SET( VERSION "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}${GIT_SHA1_VERSI
 ##### This is needed for the libyui-qt core ONLY.
 ##### These will be overridden from exports in LibyuiConfig.cmake
 SET( SONAME_MAJOR "9" )
-SET( SONAME_MINOR "1" )
+SET( SONAME_MINOR "0" )
 SET( SONAME_PATCH "0" )
 SET( SONAME "${SONAME_MAJOR}.${SONAME_MINOR}.${SONAME_PATCH}" )
